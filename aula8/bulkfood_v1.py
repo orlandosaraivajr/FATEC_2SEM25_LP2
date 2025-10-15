34ccf6d66151163f1a5ad17d1e186e6863585a70
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2025-10-15 16:29:22 -0300

    Decorators

---
+++ aula8/bulkfood_v1.py
@@ -0,0 +1,15 @@
+class LineItem:
+
+    def __init__(self, description, weight, price):
+        self.description = description
+        self.weight = weight
+        self.price = price
+
+    def subtotal(self):
+        return self.weight * self.price
+
+
+raisins = LineItem('Golden raisins', 10, 6.95)
+print(raisins.subtotal())
+raisins.weight = -20
+print(raisins.subtotal())
